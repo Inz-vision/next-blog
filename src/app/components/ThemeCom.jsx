90f2a3aae22e2d30e935f1a00fa39ce72e52d10d
Insy Hernsvan <insy.hernsvan@envisionpharma.com>
2025-04-21 14:41:01 -0400

    Add next-themes to apply dark mode

--- src/app/components/ThemeCom.jsx
+++ src/app/components/ThemeCom.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+'use client';
 
-export default function ThemeCom() {
+import { useTheme } from 'next-themes';
+
+import { useEffect, useState } from 'react';
+
+export default function ThemeCom({ children }) {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
   return (
-    <div>
-      ThemeCom
+    <div className={theme}>
+      <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-900 min-h-screen'>
+        {children}
+      </div>
     </div>
-  )
+  );
 }
